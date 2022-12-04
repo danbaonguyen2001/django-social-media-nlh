74e9d9b6390b943317744bcc3cbd4f2bc01ae90c
unknown <danbaonguyen2001@gmail.com>
2022-12-04 20:51:51 +0700

    test

--- social_book/urls.py
+++ social_book/urls.py
@@ -18,6 +18,8 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.urls import re_path as url
+from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -26,8 +28,8 @@ urlpatterns = [
     url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 ]
 
-# urlpatterns = urlpatterns+static(settings.MEDIA_URL,
-# document_root=settings.MEDIA_ROOT)
+urlpatterns = urlpatterns+static(settings.MEDIA_URL,
+document_root=settings.MEDIA_ROOT)
 
 
 # from django.contrib import admin
