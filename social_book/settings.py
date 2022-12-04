f20e79c447c9f517f5f93c235102f2b81c478159
unknown <danbaonguyen2001@gmail.com>
2022-12-04 17:12:26 +0700

    test

--- social_book/settings.py
+++ social_book/settings.py
@@ -40,11 +40,11 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
     'core',
     # # static files
-    "whitenoise.runserver_nostatic",
-    #"django.contrib.staticfiles",
+    "whitenoise.runserver_nostatic",    
+    'django.contrib.staticfiles',
+    
 ]
 
 MIDDLEWARE = [
@@ -55,9 +55,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-      # static files
-    # "django.middleware.security.SecurityMiddleware",
-    "whitenoise.middleware.WhiteNoiseMiddleware",
+      # static files    
+    "whitenoise.middleware.WhiteNoiseMiddleware",  # new
 ]
 
 ROOT_URLCONF = 'social_book.urls'
@@ -128,14 +127,19 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
-# static files
-# STATIC_HOST = "http://127.0.0.1:8000/" if not DEBUG else ""
-# STATIC_URL = STATIC_HOST + "/static/"
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR,'media')
+
+
+STATIC_URL = "/static/"
+STATICFILES_DIRS = [BASE_DIR / "static"]  
+STATIC_ROOT = BASE_DIR / "staticfiles"
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'  # new
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
+
+
 
 django_heroku.settings(locals())
 
@@ -144,5 +148,4 @@ django_heroku.settings(locals())
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR,'media')
+
--- social_book/urls.py
+++ social_book/urls.py
@@ -13,6 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
@@ -25,3 +26,23 @@ urlpatterns = [
 
 urlpatterns = urlpatterns+static(settings.MEDIA_URL,
 document_root=settings.MEDIA_ROOT)
+
+
+# from django.contrib import admin
+# from django.urls import path, include,re_path
+# from django.conf import settings
+# from django.conf.urls.static import static
+# from django.views.static import serve
+
+# urlpatterns = [
+#     path('admin/', admin.site.urls),
+#     path('', include('core.urls')),
+#     re_path(r'^media/(?P<path>.*)$',serve,{'document_root':settings.MEDIA_ROOT}),
+# ]
+
+# # urlpatterns = urlpatterns+static(settings.MEDIA_URL,
+# # document_root=settings.MEDIA_ROOT)
+
+# # if settings.DEBUG:    
+# urlpatterns += static(settings.STATIC_URL,document_root=settings.STATICFILES_DIRS[0])
+# urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
